61e4f52cc75ec58d2d11f9febc36f3a15b4fe689
jayarr <jrollock57@gmail.com>
2022-06-22 15:59:41 -0400

    Add css variable to create grids of correct number

--- javascript.js
+++ javascript.js
@@ -1,15 +1,25 @@
 
-// let input = prompt ("How big do you want your grid?");
-// if (input != null) {
+function create () {
+    function choose() {
+        const container = document.getElementById('container');
+        let ask = prompt ("How many rows do you want? Choose up to 100.") 
+            let num = ask * ask;
+            container.style.setProperty('--grid-size', ask)
+            function makeCells(num){
+                for (let i = 0; i < num; i++) {
+                    const cell = document.createElement('div');
+                    cell.className = "cell";
+                    cell.addEventListener ('mouseenter', e => {
+                        cell.style.backgroundColor = 'purple'; 
+                    });
+                    document.getElementById('container').appendChild(cell)
+                    
+            }};
+        makeCells(num);
+        }; 
+    choose();
 
-function makeCells(input){
-    for (let i = 0; i < input; i++) {
-        const cell = document.createElement('div');
-        cell.className = "cell";
-        cell.addEventListener ('mouseenter', e => {
-            cell.style.backgroundColor = 'purple'; 
-        });
-        document.getElementById('container').appendChild(cell)
-        
-}};
-makeCells(256);
+
+    
+}
+create();
