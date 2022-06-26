849c05ed826506a30054fef06dcaf1525f67dad1
jayarr <jrollock57@gmail.com>
2022-06-25 20:00:38 -0400

    Add scalable boxes and functional button

--- javascript.js
+++ javascript.js
@@ -1,25 +1,28 @@
+const button = document.createElement('button');
+button.textContent = 'How big is your canvas?';
+button.addEventListener('click', create);
+
 
 function create () {
+    const ask = prompt ("How many rows do you want? Choose up to 100.") 
     function choose() {
         const container = document.getElementById('container');
-        let ask = prompt ("How many rows do you want? Choose up to 100.") 
-            let num = ask * ask;
-            container.style.setProperty('--grid-size', ask)
-            function makeCells(num){
-                for (let i = 0; i < num; i++) {
-                    const cell = document.createElement('div');
-                    cell.className = "cell";
-                    cell.addEventListener ('mouseenter', e => {
-                        cell.style.backgroundColor = 'purple'; 
-                    });
-                    document.getElementById('container').appendChild(cell)
-                    
-            }};
-        makeCells(num);
+        
+        container.style.setProperty('--grid-size', ask)
         }; 
     choose();
 
-
-    
+    let num = ask * ask;
+    function makeCells(num){
+        for (let i = 0; i < num; i++) {
+            const cell = document.createElement('div');
+            cell.className = "cell";
+            cell.addEventListener ('mouseenter', e => {
+                cell.style.backgroundColor = 'purple'; 
+            });
+            document.getElementById('container').appendChild(cell)
+            
+    }};
+    makeCells(num);
 }
-create();
+document.body.appendChild(button);
