41d9e445daddd9efd830223a15a5fcef4d5d8b1b
jayarr <jrollock57@gmail.com>
2022-07-04 20:43:34 -0400

    finish color change scroll over

--- javascript.js
+++ javascript.js
@@ -1,28 +1,31 @@
 const button = document.createElement('button');
 button.textContent = 'How big is your canvas?';
 button.addEventListener('click', create);
+document.body.appendChild(button);
 
 
 function create () {
     const ask = prompt ("How many rows do you want? Choose up to 100.") 
     function choose() {
-        const container = document.getElementById('container');
-        
+        const container = document.getElementById('container');        
         container.style.setProperty('--grid-size', ask)
         }; 
-    choose();
-
+choose();
     let num = ask * ask;
     function makeCells(num){
         for (let i = 0; i < num; i++) {
             const cell = document.createElement('div');
             cell.className = "cell";
             cell.addEventListener ('mouseenter', e => {
-                cell.style.backgroundColor = 'purple'; 
+                let x = Math.floor(Math.random() * 256);
+                let y = Math.floor(Math.random() * 256);
+                let z = Math.floor(Math.random() * 256);
+                let randomColor = "rgb(" + x + "," + y + "," + z + ")"
+                cell.style.backgroundColor = randomColor;
             });
             document.getElementById('container').appendChild(cell)
             
-    }};
+        }
+    };
     makeCells(num);
 }
-document.body.appendChild(button);
